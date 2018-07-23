9a9a7ac3bff16027f400540a17f940e5358b3d55
William Whatley <william@catatek.com>
2018-07-23 04:48:58 -0400

    Added media query for title.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,12 +4,15 @@ import { Title1, Row, Button, I } from "../theme/index";
 
 const Div = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 400px;
   background: rgba(0, 0, 0, 0);
   display: flex;
   flex-direction: column;
   margin-left: 2em;
   margin-top: 1em;
+  @media (max-width: 500px) {
+    max-width: 300px;
+  }
 `;
 
 export default function Title({ title }) {
